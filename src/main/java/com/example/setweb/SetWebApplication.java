a85f017ce5784db23b1f9575f55f8f9a8e90b5d9
Kizureina <houchangkun@gmail.com>
2025-02-14 19:41:39 +0800

    init

---
+++ src/main/java/com/example/setweb/SetWebApplication.java
@@ -0,0 +1,12 @@
+package com.example.setweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetWebApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SetWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/setweb/SetWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.setweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SetWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
